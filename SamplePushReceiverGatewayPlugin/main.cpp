37387e52c8ec3bbb75b01f2aa4fdc37737836eee
Jonathon Williams <jwilliams@isis-jwilliams.(none)>
2010-09-02 12:36:48 -0500

    Make sample push receiver actually compile

--- SamplePushReceiverGatewayPlugin/main.cpp
+++ SamplePushReceiverGatewayPlugin/main.cpp
@@ -1,30 +1,16 @@
 #include <iostream>
 #include <string>
 
-#include "ace/INET_Addr.h"
-#include "ace/SOCK_Connector.h"
-#include "ace/SOCK_Stream.h"
-#include "ace/SOCK_Acceptor.h"
-
-#include "ace/Acceptor.h"
-
 #include "ace/Reactor.h"
 
-#include "AndroidServiceHandler.h"
+#include "GatewayConnector/GatewayConnector.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {  
-  cout << "Creating acceptor..." << endl;
+  cout << "Creating gateway connector..." << endl << flush;
   
-  //TODO: make interface and port number specifiable on the command line
-  ACE_INET_Addr serverAddress(32869, "0.0.0.0");
-  
-  cout << "Listening on port " << serverAddress.get_port_number() << " on interface " << serverAddress.get_host_addr() << endl;
-  
-  //Creates and opens the socket acceptor; registers with the singleton ACE_Reactor
-  //for accept events
-  ACE_Acceptor<AndroidServiceHandler, ACE_SOCK_Acceptor> acceptor(serverAddress);
+  GatewayConnector *gatewayConnector = new GatewayConnector(NULL);
   
   //Get the process-wide ACE_Reactor (the one the acceptor should have registered with)
   ACE_Reactor *reactor = ACE_Reactor::instance();
