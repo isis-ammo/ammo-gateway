247c5ad32ada82fa4cd97d3eaafa22e0616deef3
jonathonw <jwilliams@hotsoup.local>
2010-11-15 12:25:23 -0600

    Document the gateway plugin API.

--- LibGatewayConnector/GatewayConnector.h
+++ LibGatewayConnector/GatewayConnector.h
@@ -16,15 +16,63 @@ class PullResponseReceiverListener;
 
 class GatewayConnector {
 public:
+  /**
+  * Creates a new GatewayConnector with the given GatewayConnectorDelegate and
+  * establishes a connection to the gateway core.
+  *
+  * @param delegate A GatewayConnectorDelegate object to be used by this
+  *                 GatewayConnector instance.  May be NULL (no delegate methods
+  *                 will be called).
+  */
   GatewayConnector(GatewayConnectorDelegate *delegate);
   ~GatewayConnector();
   
   //General connection negotiation and bookkeeping
+  /**
+  * Associates a device with this GatewayConnector in the gateway core.  This
+  * method should only be needed by device connector plugins (e.g. the Android
+  * Gateway Plugin).
+  * 
+  * @todo Authentication is not yet implemented in the gateway--  this method
+  *       will send an authentication request, but the gateway will always
+  *       will always return 'success' (and this method will always return
+  *       true).  We should actually perform authentication here (pending more
+  *       information from the security people).
+  * 
+  * @param device The unique ID of the device connecting to the gateway
+  * @param user The unique ID of the user associated with the device connecting
+  *             to the gateway.
+  * @param key The authentication key/password for this user and device. Content
+  *            of this parameter is still to be determined.
+  * 
+  * @return true if authentication was successful; false if authentication
+  *         failed.
+  */
   bool associateDevice(std::string device, std::string user, std::string key);
   
   //--Data-Push support methods--
 
   //Sender-side
+  /**
+  * Pushes a piece of data (with a particular uri and type) to the gateway.
+  * 
+  * Data pushed with this method can be received by listeners registered with
+  * registerDataInterest.  All listeners registered for the type specified by
+  * mimeType will receive this peice of data.
+  * 
+  * @param uri The URI of this piece of data.  This URI should be a universally
+  *            unique identifier for the object being pushed (no two distinct
+  *            peices of data should have the same URI).
+  * @param mimeType The MIME type of this piece of data.  This MIME type is used
+  *                 to determine which other gateway plugins will receive this
+  *                 pushed data.
+  * @param data The data to be pushed to the gateway.  Represented as a C++
+  *             string; can contain any arbitrary binary data (the gateway will
+  *             accept strings with bytes invalid in C-strings such as embedded
+  *             nulls).
+  * 
+  * @return true if the operation succeeded; false if the operation failed.
+  */
   bool pushData(std::string uri, std::string mimeType, const std::string &data);
 
   bool pullRequest(std::string requestUid, std::string pluginId,
