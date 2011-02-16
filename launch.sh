7fc0b3ceeecb3845eebb843eb3041fa43033c12b
Doug Hackworth <hackd@ammo-laptop.(none)>
2011-02-16 17:21:21 -0600

    Added Jonathon's launch script in place of original placeholder script

--- launch.sh
+++ launch.sh
@@ -1,85 +1,87 @@
 #!/bin/bash
 
-function start_gateway() {
-gnome-terminal --title="Gateway" \
-  --window-with-profile="Gateway" \
-  --working-directory="${RELEASE_DIR}" \
-  --command="${RELEASE_DIR}/GatewayCore"  
-}
+LOGDIR="/tmp/gatewaylogs"
+#AMMO_PRIVATE="/opt/ammmo/ammo-private"
 
-function start_ldap_plugin() {
-gnome-terminal --title="LDAP Plugin" \
-  --window-with-profile="LDAP Plugin" \
-  --working-directory="${RELEASE_DIR}" \
-  --command="${RELEASE_DIR}/LdapGatewayPlugin"  
-}
+if pgrep -l slapd > /dev/null 2>&1 ; then
+  echo "LDAP server: running"
+else
+  echo "ERROR:  LDAP server is not running...  run it with"
+  echo "'sudo ~/ldap/ldap.sh', then rerun this script."
+  exit 1
+fi
 
-function start_ldap() {
-sudo gnome-terminal --title="LDAP" \
-  --window-with-profile="LDAP" \
-  --working-directory="${RELEASE_DIR}" \
-  --command="/opt/slapy/libexec/slapd -d 10 -f /opt/slapy/etc/openldap/slapd.conf"
-}
+if [ ! -d $LOGDIR ]; then
+  mkdir $LOGDIR
+fi
 
-function start_android_plugin() {
-gnome-terminal --title="Android Plugin" \
-  --window-with-profile="Android Plugin" \
-  --working-directory="${RELEASE_DIR}" \
-  --command="${RELEASE_DIR}/AndroidGatewayPlugin --listenPort 33289" 
-}
+hostname=`hostname`
+datesuffix=`date "+%Y.%m.%d.%H.%M.%S"`
 
-function start_tigr_plugin() {
-gnome-terminal --title="TIGR Plugin" \
-  --window-with-profile="TIGR Plugin" \
-  --working-directory="${RELEASE_DIR}" \
-  --command="${RELEASE_DIR}/TigrGatewayPlugin"  
-}
+gatewaycorelog="$LOGDIR/GatewayCore.log.$datesuffix"
+androidpluginlog="$LOGDIR/AndroidGatewayPlugin.log.$datesuffix"
+tigrpluginlog="$LOGDIR/TigrGatewayPlugin.log.$datesuffix"
+passpluginlog="$LOGDIR/PassGatewayPlugin.log.$datesuffix"
+ldappluginlog="$LOGDIR/LdapGatewayPlugin.log.$datesuffix"
 
-function start_ats_plugin() {
-gnome-terminal --title="ATS Plugin" \
-  --window-with-profile="ATS Plugin" \
-  --working-directory="${RELEASE_DIR}" \
-  --command="${RELEASE_DIR}/AtsGatewayPlugin"  
-}
+. ~/.bashrc
 
-pushd ./build/bin
-export RELEASE_DIR=$(dirs +0)
+cd $GATEWAY_ROOT/build/bin
 
-select action in 'EXIT' 'start' 'stop'; do
-  case ${action} in 
-    'EXIT') break;;
-    'start')  
-      select subaction in 'EXIT' 'All' 'LDAP' 'Gateway' \
-           'LDAP Plugin' 'Android Plugin' 'Tigr Plugin' \
-           'PASS Plugin' 'Ats Plugin'
-      do
-        case ${subaction} in
-        'EXIT') break;;
-        'All') start_gateway
-               start_android_plugin
-               start_ldap_plugin
-               start_pass_plugin
-               start_tigr_plugin
-               start_ats_plugin
-        ;;
-        'LDAP') start_ldap ;;
-        'Gateway') start_gateway ;;
-        'LDAP Plugin') start_ldap_plugin ;;
-        'Android Plugin') start_android_plugin ;;
-        'Tigr Plugin') start_tigr_plugin ;;
-        'PASS Plugin') start_pass_plugin ;;
-        'Ats Plugin') start_ats_plugin ;;
-        esac
-      done;;
-    'stop')  
-      select subaction in 'EXIT' 'All' 'LDAP' 'Gateway' \
-           'LDAP Plugin' 'Android Plugin' 'Tigr Plugin' \
-           'PASS Plugin' 'Ats Plugin'
-      do
-        case ${subaction} in
-        'EXIT') break;;
-        esac
-      done;;
-  esac
-done
-popd
+echo "Launching Gateway Core..."
+echo "  Log file in $gatewaycorelog"
+./GatewayCore > $gatewaycorelog 2>&1 &
+xterm -bg black -fg orange -sb -title "Gateway Core ($HOSTNAME)" -e "tail -n+0 -f $gatewaycorelog" &
+
+sleep 5
+
+echo "Launching Android Gateway Plugin..."
+echo "  Log file in $androidpluginlog"
+./AndroidGatewayPlugin --listenPort 33289 > $androidpluginlog 2>&1 &
+xterm -bg black -fg cyan -sb -title "Android Gateway Plugin ($HOSTNAME)" -e "tail -n+0 -f $androidpluginlog" &
+
+sleep 5
+
+#pushd $AMMO_PRIVATE/TigrGatewayPlugin/
+
+export LD_LIBRARY_PATH=$AMMO_PRIVATE/TigrGatewayPlugin/:$LD_LIBRARY_PATH
+
+echo "Launching TIGR Gateway Plugin..."
+echo "  Log file in $tigrpluginlog"
+./TigrGatewayPlugin > $tigrpluginlog 2>&1 &
+xterm -bg black -fg yellow -sb -title "TIGR Gateway Plugin ($HOSTNAME)" -e "tail -n+0 -f $tigrpluginlog" &
+
+#popd
+
+sleep 5
+
+#pushd $AMMO_PRIVATE/PassGatewayPlugin/
+
+echo "Launching PASS Gateway Plugin..."
+echo "  Log file in $passpluginlog"
+./PassGatewayPlugin > $passpluginlog 2>&1 &
+xterm -bg black -fg gray -sb -title "PASS Gateway Plugin ($HOSTNAME)" -e "tail -n+0 -f $passpluginlog" &
+
+#popd
+
+sleep 5
+
+echo "Launching LDAP Gateway Plugin..."
+echo "  Log file in $ldappluginlog"
+./LdapGatewayPlugin > $ldappluginlog 2>&1 &
+echo ""
+echo "Type Control+C to exit."
+xterm -bg black -fg green -sb -title "LDAP Gateway Plugin ($HOSTNAME)" -e "tail -n+0 -f $ldappluginlog"
+
+echo "Terminating gateway applications..."
+
+echo "Terminating Gateway Core..."
+kill %./Gatewa
+echo "Terminating Android Gateway Plugin..."
+kill %./AndroidGat
+echo "Terminating TIGR Gateway Plugin..."
+kill %./TigrGat
+echo "Terminating PASS Gateway Plugin..."
+kill %./PassGat
+echo "Terminating LDAP Gateway Plugin..."
+kill %./LdapGat
