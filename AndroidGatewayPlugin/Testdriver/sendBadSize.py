a23a64233c818281c1d4582fcf7260a1da16a949
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2011-02-24 13:57:34 -0600

    Test for behavior on a bad checksum and bad (too large) size (as might happen if the device sends invalid data).

---
+++ AndroidGatewayPlugin/Testdriver/sendBadSize.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+#Test driver for Android gateway plugin to test deserialization of messages.
+
+import sys
+import socket
+import struct
+import zlib
+import random
+
+import AmmoMessages_pb2
+
+class GatewayTestClient:
+  def __init__(self, host, port):
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.sock.connect((host, int(port)))
+    
+  def sendMessageWrapper(self, msg):
+    serializedMsg = msg.SerializeToString()
+    self.sock.sendall(struct.pack("<I", 4000000000)) #little-endian byte order for now
+    self.sock.sendall(struct.pack("<i", zlib.crc32(serializedMsg)))
+    print serializedMsg
+    self.sock.sendall(serializedMsg);
+    
+  def receiveMessage(self):
+    (messageSize,) = struct.unpack("<I", self.sock.recv(4));
+    (checksum,) = struct.unpack("<i", self.sock.recv(4));
+    protobufMsg = ""
+    while len(protobufMsg) < messageSize:
+      receivedData = self.sock.recv(messageSize - len(protobufMsg))
+      protobufMsg += receivedData
+    calculatedChecksum = zlib.crc32(protobufMsg)
+    if calculatedChecksum != checksum:
+      print "Checksum error!"
+      return None
+    msg = AmmoMessages_pb2.MessageWrapper()
+    msg.ParseFromString(protobufMsg)
+    return msg
+
+if __name__ == "__main__":
+  print "Android Gateway Tester"
+  if len(sys.argv) != 4:
+    print "Usage:", sys.argv[0], "host port message-type"
+    print '''
+  where message-type is one of:"
+    authenticate : always run, this a dummy actually anything would work.
+    subscribe : subscribe to type:edu.vanderbilt.isis.ammo.Test.
+    push : send a data message of topic type:edu.vanderbilt.isis.ammo.Test.
+'''
+    exit(-1)
+  
+  print "Creating client"
+  client = GatewayTestClient(sys.argv[1], sys.argv[2])
+  print "Generating message"
+  m = AmmoMessages_pb2.MessageWrapper()
+  m.type = AmmoMessages_pb2.MessageWrapper.AUTHENTICATION_MESSAGE
+  m.authentication_message.device_id = "device:test/device1"
+  m.authentication_message.user_id = "user:test/user1"
+  m.authentication_message.user_key = "dummy"
+  print "Sending message"
+  client.sendMessageWrapper(m)
+  
+  if(sys.argv[3] == "push"):
+    #wait for auth response, then send a data push message
+    #response = client.receiveMessage()
+    #if response.authentication_result.result != AmmoMessages_pb2.AuthenticationResult.SUCCESS:
+    #  print "Authentication failed..."
+    m = AmmoMessages_pb2.MessageWrapper()
+    m.type = AmmoMessages_pb2.MessageWrapper.DATA_MESSAGE
+    m.data_message.uri = "type:edu.vanderbilt.isis.ammo.Test"
+    m.data_message.mime_type = "text/plain"
+    m.data_message.data = "This is some text being pushed out to the gateway."
+    print "Sending data message"
+    client.sendMessageWrapper(m)
+  elif sys.argv[3] == "subscribe": 
+    #wait for auth response, then send a data push message
+    response = client.receiveMessage()
+    if response.authentication_result.result != AmmoMessages_pb2.AuthenticationResult.SUCCESS:
+      print "Authentication failed..."
+    m = AmmoMessages_pb2.MessageWrapper()
+    m.type = AmmoMessages_pb2.MessageWrapper.SUBSCRIBE_MESSAGE
+    m.subscribe_message.mime_type = "text/plain"
+    print "Sending subscription request..."
+    client.sendMessageWrapper(m)
+    
+  
+  while True:
+    msg = client.receiveMessage()
+    print msg
+  
+  print "Closing socket"
+  
