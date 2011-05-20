304f460b0e24b17f24548a18085dc9f89bba1f65
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2011-05-20 12:56:51 -0500

    Handle SIGINT and SIGTERM in gateway and plugins.  refs #253

--- LocationStoreGatewayPlugin/main.cpp
+++ LocationStoreGatewayPlugin/main.cpp
@@ -1,10 +1,25 @@
 #include "ace/Reactor.h"
 
+#include "ace/OS_NS_unistd.h" 
+#include "ace/Signal.h" 
+
 #include "log.h"
 #include "version.h"
 
 #include "LocationStore.h"
 #include "LocationStoreConfigManager.h"
+
+//Handle SIGINT so the program can exit cleanly (otherwise, we just terminate
+//in the middle of the reactor event loop, which isn't always a good thing).
+class SigintHandler : public ACE_Event_Handler {
+public:
+  int handle_signal (int signum, siginfo_t * = 0, ucontext_t * = 0) {
+    if (signum == SIGINT || signum == SIGTERM) {
+      ACE_Reactor::instance()->end_reactor_event_loop();
+    }
+    return 0;
+  }
+};
 
 int main (int /* argc */, char ** /* argv */)
 {
@@ -15,6 +30,10 @@ int main (int /* argc */, char ** /* argv */)
             << " at "
             << __TIME__
             << ")");
+  
+  SigintHandler * handleExit = new SigintHandler();
+  ACE_Reactor::instance()->register_handler(SIGINT, handleExit);
+  ACE_Reactor::instance()->register_handler(SIGTERM, handleExit);
   
   LOG_DEBUG ("Creating location store receiver...");
   
