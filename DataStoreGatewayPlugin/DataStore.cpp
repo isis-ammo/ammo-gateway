ec78bd3785411e6bf29ebd3ea317ec6070c66abc
Jeff Parsons <j.parsons@vanderbilt.edu>
2011-07-29 11:21:10 -0500

    added append of forward slash to DataStore db filename path if not already supplied

--- DataStoreGatewayPlugin/DataStore.cpp
+++ DataStoreGatewayPlugin/DataStore.cpp
@@ -88,6 +88,11 @@ void
 DataStoreReceiver::db_filepath (const std::string &path)
 {
   db_filepath_ = path;
+  
+  if (db_filepath_[db_filepath_.size () - 1] != '/')
+    {
+      db_filepath_.push_back ('/');
+    }
 }
 
 bool
