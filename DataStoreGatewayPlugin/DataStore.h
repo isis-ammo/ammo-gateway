3e247ec9628261025194b3387bf804d1fc364c45
Jeff Parsons <j.parsons@vanderbilt.edu>
2011-08-11 11:55:55 -0500

    restored lost changes to DataStore.{h,cpp}

--- DataStoreGatewayPlugin/DataStore.h
+++ DataStoreGatewayPlugin/DataStore.h
@@ -3,6 +3,8 @@
 
 #include "GatewayConnector.h"
 
+#include "DataStoreDispatcher.h"
+
 class sqlite3;
 class sqlite3_stmt;
 
@@ -36,52 +38,12 @@ public:
   bool init (void);
 
 private:
-  struct PrivateContact 
-  {
-    std::string first_name;
-    std::string middle_initial;
-    std::string last_name;
-    std::string rank;
-    std::string call_sign;
-    std::string branch;
-    std::string unit;
-    std::string email;
-    std::string phone;
-    std::vector<unsigned char> photo;
-    std::vector<unsigned char> insignia;
-  };
+  // Dispatcher class that handles all pushes and pulls.
+  DataStoreDispatcher dispatcher_;
 
-  bool pushContactData (const ammo::gateway::PushData &pushData);
-  bool pushGeneralData (const ammo::gateway::PushData &pushData);
-
-  bool parseJson (const std::string &input, Json::Value &root);
-  void contactFromJson (const Json::Value &jsonRoot, PrivateContact &info);
-  
-  bool createTable (const char *tbl_string, const char *msg);
-  
-  bool bind_text (int column,
-                  const std::string &text,
-                  const char *push_type,
-                  const char *column_name);
-                  
-  bool bind_int (int column,
-                 int val,
-                 const char *push_type,
-                 const char *column_name);
-                 
-  bool bind_blob (int column,
-                  const void *val,
-                  int size,
-                  const char *push_type,
-                  const char *column_name);
-                  
-private:
   // Pointer to open database.
   sqlite3 *db_;
   
-  // Temporary holder for statements being constructed.
-  sqlite3_stmt *stmt_;
-	
   // Set by the config manager.
   std::string db_filepath_;
 };
