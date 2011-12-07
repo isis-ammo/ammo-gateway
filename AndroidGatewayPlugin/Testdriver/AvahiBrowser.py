d3f0286d7a02f39e690ff4ac8ae9e63aacaf1ea5
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2011-12-07 16:03:37 -0600

    Only search for IPV4 gateways (the gateway doesn't work over IPv6).
    This really should be fixed on the service registration side, but
    the Bonjour API doesn't appear to provide a way for me to specify
    that it should only register for IPV4.

--- AndroidGatewayPlugin/Testdriver/AvahiBrowser.py
+++ AndroidGatewayPlugin/Testdriver/AvahiBrowser.py
@@ -8,13 +8,13 @@ TYPE = '_ammogateway._tcp'
 
 class AvahiBrowser:
   def service_resolved(self, *args):
-      print 'service resolved'
-      print 'name:', args[2]
-      print 'address:', args[7]
-      print 'port:', args[8]
-      self.serverAddress = args[7]
-      self.serverPort = args[8]
-      self.mainLoop.quit()
+    print 'Service resolved:'
+    print '  name:', args[2]
+    print '  address:', args[7]
+    print '  port:', args[8]
+    self.serverAddress = args[7]
+    self.serverPort = args[8]
+    self.mainLoop.quit()
   
   def print_error(self, *args):
       print 'error_handler'
@@ -28,7 +28,7 @@ class AvahiBrowser:
               pass
   
       self.server.ResolveService(interface, protocol, name, stype, 
-          domain, avahi.PROTO_UNSPEC, dbus.UInt32(0), 
+          domain, avahi.PROTO_INET, dbus.UInt32(0), 
           reply_handler=self.service_resolved, error_handler=self.print_error)
   
   def searchForServices(self):
@@ -41,12 +41,13 @@ class AvahiBrowser:
     
     self.sbrowser = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME,
             self.server.ServiceBrowserNew(avahi.IF_UNSPEC,
-                avahi.PROTO_UNSPEC, TYPE, 'local', dbus.UInt32(0))),
+                avahi.PROTO_INET, TYPE, 'local', dbus.UInt32(0))),
             avahi.DBUS_INTERFACE_SERVICE_BROWSER)
     
     self.sbrowser.connect_to_signal("ItemNew", self.myhandler)
     
     self.mainLoop = gobject.MainLoop()
+    print "Looking for a gateway via Avahi..."
     self.mainLoop.run()
     
     return (self.serverAddress, int(self.serverPort))
