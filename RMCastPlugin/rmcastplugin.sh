4345e47eb1a87b8e29da8318c94dda8ca538c550
John Williams <johnwilliams@isis.vanderbilt.edu>
2012-04-23 17:41:30 -0500

    fixed path error in linux launchers for mcast and rmcast

--- RMCastPlugin/rmcastplugin.sh
+++ RMCastPlugin/rmcastplugin.sh
@@ -1,1 +1,1 @@
-java -Djava.net.preferIPv4Stack=true  -cp gatewaypluginapi.jar:slf4j-api-1.6.4.jar:slf4j-simple-1.6.4.jar:jgroups-gw.jar:json-20090211.jar:protobuf-java-2.3.0.jar:dist/lib/rmcastplugin.jar edu.vu.isis.ammo.rmcastplugin.RMCastPlugin
+java -Djava.net.preferIPv4Stack=true  -cp gatewaypluginapi.jar:slf4j-api-1.6.4.jar:slf4j-simple-1.6.4.jar:jgroups-gw.jar:json-20090211.jar:protobuf-java-2.3.0.jar:rmcastplugin.jar edu.vu.isis.ammo.rmcastplugin.RMCastPlugin
