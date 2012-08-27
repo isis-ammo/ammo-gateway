05e4114b4096f44b0644e4a9487face4fd17ca7e
John Williams <johnwilliams@isis.vanderbilt.edu>
2012-08-27 11:13:20 -0500

    fixes for javaservice for 64-bit windows and 64-bit java

--- MCastPlugin/mcastplugin-install.bat
+++ MCastPlugin/mcastplugin-install.bat
@@ -6,6 +6,11 @@ rem
 rem Installation may be corrupted during Java upgrades because the location
 rem of jvm.dll moves. In this case, run the corresponding uninstallation
 rem script followed by this script again to correct the situation.
+rem
+rem Error Codes:
+rem    1 - Could not find CurVer of Java in Windows registry
+rem    2 - Could not find location of JVM DLL in Windows registry
+rem    3 - Could not find JVM DLL at location as registered or alternative
 rem **************************************************************************
 
 echo Installing AMMO MCastPlugin ...
@@ -14,9 +19,9 @@ rem Detect Java's current version for registry crawling
 FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment" /v CurrentVersion') DO set CurVer=%%B
 if not defined CurVer (
   echo ERROR: Java not found - Failed to find CurVer
-  echo Skipping AMMO RMCastPlugin installation
-  echo You must install Java before running rmcastplugin-install.bat
-  goto end
+  echo Skipping AMMO MCastPlugin installation
+  echo You must install Java before running mcastplugin-install.bat
+  exit /b 1
 )
 echo Found Java version: %CurVer%
 
@@ -24,9 +29,18 @@ rem Detect Java's jvm.dll
 FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment\%CurVer%" /v RuntimeLib') DO set JVM_DLL=%%B
 if not defined JVM_DLL (
   echo ERROR: Java not found - Failed to find RuntimeLib
-  echo Skipping AMMO RMCastPlugin installation
-  echo You must install Java before running rmcastplugin-install.bat
-  goto end
+  echo Skipping AMMO MCastPlugin installation
+  echo You must install Java before running mcastplugin-install.bat
+  exit /b 2
+)
+if not exist "%JVM_DLL%" (
+  set JVM_DLL=%JVM_DLL:client=server%
+)
+if not exist "%JVM_DLL%" (
+  echo ERROR: Java not found - JVM does not appear at the location declared in the registry nor the alternate location.
+  echo Skipping AMMO MCastPlugin installation
+  echo You must install Java before running mcastplugin-install.bat
+  exit /b 3
 )
 echo Found jvm.dll at %JVM_DLL%
 
@@ -47,5 +61,5 @@ JavaService -install "AMMO MCast Plugin" "%JVM_DLL%" ^
 
 popd
 
-:end
+exit /b 0
 
