93ae0b9aa76c5c17e90f8931bffd5db7542a47f4
John Williams <johnwilliams@isis.vanderbilt.edu>
2012-11-14 16:46:22 -0600

    Add protobuf 2.4.1 jar file to install scripts. Add QTDIR to build script.

--- scripts/gateway-exe.ps1
+++ scripts/gateway-exe.ps1
@@ -11,7 +11,7 @@ Write-Host GATEWAY_ROOT: $env:GATEWAY_ROOT
 cd $env:GATEWAY_ROOT
 ant configure
 ant build
-& "C:\Program Files\NSIS\makensis.exe" /DVERSION=$env:GATEWAY_VERSION /DACE_ROOT=$env:ACE_ROOT /DPROTOBUF_ROOT=$env:PROTOBUF_ROOT ammo-gateway.nsi
+& "C:\Program Files\NSIS\makensis.exe" /DVERSION=$env:GATEWAY_VERSION /DACE_ROOT=$env:ACE_ROOT /DPROTOBUF_ROOT=$env:PROTOBUF_ROOT /DQTDIR="C:\Qt\4.8.3" ammo-gateway.nsi
 
 mv "ammo-gateway-$(get_version_full).exe" "$(get_version_branch)-ammo-gateway-$(get_version_full).exe"
 
