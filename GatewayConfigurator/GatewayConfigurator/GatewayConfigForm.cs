64c529704d6eb437c9e55abe2a3a998fcf247550
U-jwmac\jwilliams <jwilliams@jwmac.(none)>
2013-01-31 13:43:48 -0600

    Add C Sharp gateway config UI

---
+++ GatewayConfigurator/GatewayConfigurator/GatewayConfigForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GatewayConfigurator
+{
+    public partial class GatewayConfigForm : Form
+    {
+        public GatewayConfigForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
