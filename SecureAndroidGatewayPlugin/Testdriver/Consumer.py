e61858c510f74ec5121582ea980a74817496abcb
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2013-06-12 16:57:38 -0500

    Add SSL testdrivers

---
+++ SecureAndroidGatewayPlugin/Testdriver/Consumer.py
@@ -0,0 +1,84 @@
+from ammo import SecureAndroidConnector
+from ammo import AmmoMessages_pb2
+
+import uuid
+import sys
+import time
+import datetime
+import math
+import optparse
+
+
+from twisted.internet import reactor
+
+latencies = []
+
+def onDataReceived(connector, msg):
+  receivedTime = time.time()
+  if msg.type == AmmoMessages_pb2.MessageWrapper.DATA_MESSAGE:
+    splitMessage = msg.data_message.data.split("/")
+    sequenceNumber = int(splitMessage[0])
+    sentTime = float(splitMessage[1])
+    timeDifference = receivedTime - sentTime
+    print "{0},{1:.9f}".format(sequenceNumber, timeDifference)
+    latencies.append(timeDifference)
+
+if __name__ == "__main__":
+  print "Android Gateway Tester"
+  
+  parser = optparse.OptionParser()
+  parser.add_option("-g", "--gateway", dest="gateway",
+                    help="Gateway to connect to (default %default)",
+                    default="127.0.0.1")
+  parser.add_option("-p", "--port", dest="port", type="int",
+                    help="Gateway port to connect to (default %default)",
+                    default=33290)
+  parser.add_option("-s", "--scope", dest="scope",
+                    help="Subscription scope (either local or global; default %default)",
+                    default="global")
+  
+  (options, args) = parser.parse_args()
+  
+  scope = SecureAndroidConnector.MessageScope.GLOBAL
+  if options.scope == "local":
+    scope = SecureAndroidConnector.MessageScope.LOCAL
+  elif options.scope == "global":
+    scope = SecureAndroidConnector.MessageScope.GLOBAL
+  else:
+    print "scope must be one of: local global"
+    exit(-1)
+  
+  deviceName = "device:test/" + uuid.uuid1().hex
+  userName = "user:test/" + uuid.uuid1().hex
+  
+  connector = SecureAndroidConnector.SecureAndroidConnector(options.gateway, options.port, deviceName, userName, "")
+  connector.setMessageQueueEnabled(False)
+  connector.registerMessageCallback(onDataReceived)
+  
+  try:
+    connector.start()
+    connector.waitForAuthentication()
+    
+    print "Subscribing."
+    connector.subscribe("ammo/edu.vu.isis.ammo.test.TestData", scope)
+    sequenceNumber = 0
+    
+    while True:
+      time.sleep(0.5)
+      
+  except KeyboardInterrupt:
+    print "Got ^C...  Closing"
+    reactor.callFromThread(reactor.stop)
+    print "MeanLatency,StdDev,Samples"
+    if len(latencies) > 0:
+      meanLatency = sum(latencies) / len(latencies)
+      squaredDifferences = [(x - meanLatency) ** 2 for x in latencies]
+      variance = sum(squaredDifferences) / len(latencies)
+      stddev = math.sqrt(variance)
+      print str(meanLatency) + "," + str(stddev) + "," + str(len(latencies))
+    else:
+      print "0,0"
+  except:
+    print "Unexpected error...  dying."
+    reactor.callFromThread(reactor.stop)
+    raise
