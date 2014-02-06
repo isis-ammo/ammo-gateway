7700d306cf4dbb32c4cace0a21b519f7c91975fa
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2014-02-06 13:44:36 -0600

    Add skeleton for couchdb datastore plugin

---
+++ CouchDBDatastorePlugin/src/main/java/edu/vu/isis/ammo/gateway/cdb/Main.java
@@ -0,0 +1,15 @@
+package edu.vu.isis.ammo.gateway.cdb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by jwilliams on 2/6/14.
+ */
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Couch DB Datastore Plugin");
+    }
+}
