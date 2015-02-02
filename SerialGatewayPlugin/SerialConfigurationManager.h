34dcdb8373383cbd6266b893d7bd8466bd09a46a
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2015-02-02 15:52:45 -0600

    Update license

--- SerialGatewayPlugin/SerialConfigurationManager.h
+++ SerialGatewayPlugin/SerialConfigurationManager.h
@@ -1,29 +1,22 @@
-/* Copyright (C) 2010-2013 Vanderbilt University
- *  
- * This software was developed by the Institute for Software Integrated
- * Systems (ISIS) at Vanderbilt University, Tennessee, USA for the
- * Transformative Apps program under DARPA, Contract # HR011-10-C-0175 and
- * Contract # HR0011-12-C-0109.
- *  
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this data, including any software or models in source or binary
- * form, as well as any drawings, specifications, and documentation
- * (collectively "the Data"), to deal in the Data without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Data, and to
- * permit persons to whom the Data is furnished to do so, subject to the
- * following conditions:
- *  
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Data.
- *  
- * THE DATA IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/* Copyright (c) 2010-2015 Vanderbilt University
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS, SPONSORS, DEVELOPERS, CONTRIBUTORS, OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE DATA OR THE USE OR OTHER DEALINGS IN THE DATA.
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 #ifndef SERIAL_CONFIGURATION_MANAGER_H
